7940d240184cb142ca690f403b06d52498b478ac
arafathsiam99 <siammdarafathislam@gmail.com>
2023-04-05 23:15:33 +0600

    1 file modified

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ import './style.css';
 // Write Javascript code!
 console.log('Hello Friends!!');
 
+const syncWithCallback = () => {
+  for (let index = 0; index < 10; index++) {
+    console.log('looping: \t', index, '\t', new Date());
+  }
+};
+syncWithCallback();
+console.log('all done');
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Learn Mern like a pro</h1>`;
