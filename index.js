db116373712cd4c71967103a8c40d706fdaad3a8
arafathsiam99 <siammdarafathislam@gmail.com>
2023-04-05 23:08:55 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 
 // Write Javascript code!
-console.log('Hello Guys!!');
+console.log('Hello Friends!!');
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Learn Mern like a pro</h1>`;
