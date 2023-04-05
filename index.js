3b8a61bf26e63baed29c8ceb5aa295bac4397b53
arafathsiam99 <siammdarafathislam@gmail.com>
2023-04-05 23:32:16 +0600

    1 file modified

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ import './style.css';
 // Write Javascript code!
 console.log('Hello Friends!!');
 
+const isPrime =(num)=>{
+  if(num<2) return false;
+  for(let i =2;i<num;i++){
+    if(num%i===0)return false;
+  }
+  return true;
+}
+
+
+const syncCallbackFunction = (number,start)=>{
+  const numIsPrime= isPrime(number);
+  console.log('isprime: ',number,'result: ',numIsPrime,'start',start,'end', new Date());
+}
+
+const sleepWithCallback = (number,callback)=>{
+  const start = Date.now();
+  const sleep = number*1000;
+  while(true){
+    if(Date.now()-start>sleep) break;
+  }
+  callback(number,new Date(start));
+}
+
 const syncWithCallback = () => {
   for (let index = 0; index < 10; index++) {
     console.log('looping: \t', index, '\t', new Date());
+    sleepWithCallback(index,syncCallbackFunction);
   }
 };
 syncWithCallback();
